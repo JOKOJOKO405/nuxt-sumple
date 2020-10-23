e0424d9d29fef77ee386b4e53a799c4c77275c83
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-23 10:48:00 +0900

    エレメントUIで遊んだ

---
+++ pages/element_test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>
+      エレメント使用のテストページ
+    </h1>
+    <el-button @click="visible = true">エレメントボタン</el-button>
+    <el-dialog :visible.sync="visible" title="確認">
+      <p>処理されましたよ！</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      visible: false,
+    }
+  }
+}
+</script>
