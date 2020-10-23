abff116c7caad2518669180c54d6f02c97e000f9
JOKOJOKO405 <biz.kj405@gmail.com>
2020-10-23 11:01:59 +0900

    axiosでIP取得

---
+++ pages/axios.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ ip }}</p>
+    <button @click="getIp">IPアドレス取得</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      ip: '127.0.0.1',
+    }
+  },
+  methods: {
+    async getIp(){
+      this.ip = await this.$axios.$get('http://icanhazip.com')
+    }
+  }
+}
+</script>
